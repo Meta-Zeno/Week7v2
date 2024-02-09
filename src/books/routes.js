d52fd105da70ec5bb9b1aba873945a9567ffd7a0
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-09 10:45:02 +0000

    finding errors to fix thunder client

--- src/books/routes.js
+++ src/books/routes.js
@@ -2,27 +2,26 @@ const { Router } = require("express");
 const bookRouter = Router();
 const Book = require("./model");
 
+const { getAllBooks } = require("./controllers");
 const { addBook } = require("./controllers");
 const { findBook } = require("./controllers");
 const { getFirstBook } = require("./controllers");
 const { updateAuthor } = require("./controllers");
 const { findOneAndUpdate } = require("./controllers");
 const { deleteResult } = require("./controllers");
-const { getAllBooks } = require("./controllers");
 
 //Create a new book entry in the database using POST request function.
 //##############################################
-bookRouter.post("/books/addbook", addBook);
-bookRouter.get("/books/findBooks", findBook);
+bookRouter.get("/books/getAllBooks", getAllBooks);
+bookRouter.post("/books/addBook", addBook);
+bookRouter.get("/books/findBook", findBook);
 bookRouter.get("/books/getFirstBook", getFirstBook);
 bookRouter.put("/books/updateAuthor", updateAuthor);
-bookRouter.put("/books/updateAuthor", findOneAndUpdate);
-bookRouter.put("/books/updateAuthor", deleteResult);
-bookRouter.put("/books/updateAuthor", getAllBooks);
-
+bookRouter.put("/books/findOneAndUpdate", findOneAndUpdate);
+bookRouter.delete("/books/deleteResult", deleteResult);
 //###############################################
 //anatomy of a route
 
-// bookRouter.httpMethod(route, fuinction)
+// bookRouter.httpMethod(route, function)
 
 module.exports = bookRouter;
